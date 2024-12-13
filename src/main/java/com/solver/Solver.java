74e2933f9a4cbdb5bf1a1f872e1ab0418972b817
Tigran Oganesyan <66678052+javaCoder1707@users.noreply.github.com>
2024-12-13 22:11:07 +0300

    Update Solver.java

--- src/main/java/com/solver/Solver.java
+++ src/main/java/com/solver/Solver.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class Solver {
     public static void main(String[] args) throws IOException {
-        Searcher searcherOfFiles = new Searcher("C:/Users/lenovo/Desktop/TZ");
+        Searcher searcherOfFiles = new Searcher("PATH_TO_MAIN_DIRECTORY");
         Sorter sorterOfFiles = new Sorter(searcherOfFiles.search());
         System.out.println(sorterOfFiles.sort());
     }
